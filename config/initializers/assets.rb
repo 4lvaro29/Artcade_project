f3dfecb1c23930e51a0c9ee2202c2e6c0cb54e04
felipe vera <felipevera10@gmail.com>
2017-10-05 19:06:37 -0300

    template added

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets")
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "javascripts")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "images")
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
