7d58ff3ec32edf506fb0b88b9ce3a72238c7442b
Matias Seguel <mseguel@mapcity.cl>
2017-10-11 12:42:49 -0300

    added index arcades

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,6 @@
 class PagesController < ApplicationController
   def home
-  	@arcades = Arcade.all
-  
-  	@components = Component.all
-  	@items = @arcades + @components
-  	@items.shuffle!
+  	@arcades = Arcade.all.suffle
   end
 
   def about
