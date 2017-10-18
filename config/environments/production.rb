f4a0c2bde307712436617d10f1ed9804dc28bbca
Alvaro Urbina <4lvaro.up@gmail.com>
2017-10-18 16:05:55 -0300

    reset app pass mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "artcade_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.deafult_url_options = {:host => 'https://artcade.herokuapp.com/'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
